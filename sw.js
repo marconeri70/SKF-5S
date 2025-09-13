0d9fb5e73c6b2d88a44456ce518c6b85407018f4
marconeri70 <marconeri70@gmail.com>
2025-09-13 11:49:48 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-// SKF 5S PWA SW – v7.7.0
-const CACHE_NAME = "skf5s-cache-v10";
+// SKF 5S PWA SW – v7.8.0
+const CACHE_NAME = "skf5s-cache-v12";
 const FILES_TO_CACHE = [
   "./",
   "./index.html",
-  "./style.css?v=7.7.0",
-  "./app.js?v=7.7.0",
-  "./manifest.json?v=7.7.0",
+  "./style.css?v=7.8.0",
+  "./app.js?v=7.8.0",
+  "./manifest.json?v=7.8.0",
   "./assets/skf-logo.png",
   "./assets/skf-192.png",
   "./assets/skf-512.png"
