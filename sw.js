8556ffe5b217b0beb1e63560046351c1232818fb
marconeri70 <marconeri70@gmail.com>
2025-09-15 10:51:53 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,30 +1,19 @@
-const CACHE = 'skf5s-v7.15.3';
-const ASSETS = [
-  './',
-  'index.html',
-  'style.css?v=7.15.3',
-  'app.js?v=7.15.3',
-  'assets/5S.png',
-  'assets/skf-192.png'
+// Cache super semplice per PWA offline
+const SW_VERSION = '7.16.0';
+const CORE = [
+  './','index.html','style.css?v=7.16.0','app.js?v=7.16.0',
+  'assets/skf-logo.png','assets/skf-192.png','assets/skf-512.png','assets/5S.png'
 ];
 
-self.addEventListener('install', (e)=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
-  self.skipWaiting();
+self.addEventListener('install', e=>{
+  e.waitUntil(caches.open(SW_VERSION).then(c=>c.addAll(CORE)));
 });
-self.addEventListener('activate', (e)=>{
-  e.waitUntil(
-    caches.keys().then(keys=>Promise.all(keys.map(k=> k!==CACHE && caches.delete(k))))
-  );
-  self.clients.claim();
+self.addEventListener('activate', e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==SW_VERSION).map(k=>caches.delete(k)))));
 });
-self.addEventListener('fetch', (e)=>{
-  const {request} = e;
-  e.respondWith(
-    caches.match(request).then(res => res || fetch(request).then(r=>{
-      const copy = r.clone();
-      caches.open(CACHE).then(c=>c.put(request, copy)).catch(()=>{});
-      return r;
-    }).catch(()=> caches.match('./')))
-  );
+self.addEventListener('fetch', e=>{
+  const url = new URL(e.request.url);
+  if(url.origin===location.origin){
+    e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+  }
 });
