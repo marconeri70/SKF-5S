63a897ede45e10dcca6e0b772f6ccd8b78477a09
marconeri70 <marconeri70@gmail.com>
2025-09-27 12:45:12 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,15 +1,1 @@
-
-const CACHE='skf5s-supervisor-v211';
-const FILES=[
-  './','./index.html','./checklist.html','./notes.html','./style.css','./app.js','./manifest.json',
-  './assets/5s.png','./assets/skf-192.png','./assets/skf-512.png','./assets/skf-logo.png'
-];
-self.addEventListener('install',e=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));
-});
-self.addEventListener('activate',e=>{
-  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
-});
-self.addEventListener('fetch',e=>{
-  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
-});
+self.addEventListener('install',e=>self.skipWaiting());self.addEventListener('activate',e=>clients.claim());
