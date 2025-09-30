d53be5abe5e6a5d1177040f1e684dbe4ce7fd0bf
marconeri70 <marconeri70@gmail.com>
2025-09-30 10:33:34 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,1 @@
-self.addEventListener('install', e=>{ self.skipWaiting(); });
-self.addEventListener('activate', e=>{ self.clients.claim(); });
-self.addEventListener('fetch', e=>{
-  const url = new URL(e.request.url);
-  if (url.origin === location.origin) {
-    e.respondWith(fetch(e.request).catch(()=>caches.match('/index.html')));
-  }
-});
+self.addEventListener('fetch',()=>{});
