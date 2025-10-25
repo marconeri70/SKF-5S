355a6d9cf9dfbc16e75b355d84266c03b4cdabf8
marconeri70 <marconeri70@gmail.com>
2025-10-25 16:42:29 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-// v2.3.5 — SW neutro: nessuna cache aggressiva
+// v2.3.6 — SW neutro: nessuna cache aggressiva
 self.addEventListener('install', e => self.skipWaiting());
 self.addEventListener('activate', e => self.clients.claim());
-// niente fetch: usiamo sempre la rete → i file nuovi sono immediati
+// niente fetch handler → usa sempre i file più recenti
